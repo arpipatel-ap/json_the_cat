6d71c079a27ba7eab6ddfcb250ffc27153aca17c
arpipatel-ap <arpi96@gmail.com>
2024-05-10 01:11:56 -0400

    Refactry file and add unit testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,33 +2,34 @@ const needle = require('needle');
 const breedName = process.argv[2];
 
 
-if (!breedName) {
-  console.error('Please provide a breed name.');
-  process.exit(1);
-}
-
-
-const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-needle.get(apiUrl, function(error, response) {
-  if (error) {
-    console.error('Error fetching data:', error);
-    process.exit(1); // Exit with error
+const fetchBreedDescription = function(breedName, callback) {
+  // Check if breed name is provided
+  if (!breedName) {
+    return callback('Please provide a breed name.', null);
   }
 
-  if (response.statusCode !== 200) {
-    console.error('Request failed with status code:', response.statusCode);
-    process.exit(1); // Exit with error
-  }
 
-  const responseBody = JSON.parse(response.body);
 
+  const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  needle.get(apiUrl, function(error, response) {
+    if (error) {
+      return callback('Error fetching data:' + error, null);
+   
+    }
+
+    if (response.statusCode !== 200) {
+      return callback('Request failed with status code:' + response.statusCode, null);
+    }
+
+    const responseBody = JSON.parse(response.body);
     
-  if (responseBody.length === 0) {
-    console.log(`Breed '${breedName}' not found.`);
-    process.exit(0); // Exit successfully
-  }
+    if (responseBody.length === 0) {
+      return callback(`Breed '${breedName}' not found.`, null);
+    }
 
-  const description = responseBody[0].description;
-  console.log(description);
-});
+    const description = responseBody[0].description;
+    callback(description);
+  });
+};
+module.exports = { fetchBreedDescription };
